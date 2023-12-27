8591750652295e8555e541d70260190224484ba3
linusTC <tongchun03@gmail.com>
2023-12-27 14:01:13 -0800

    hmm

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ const group = new THREE.Group();
 group.add(globe);
 scene.add(group);
 const rotationSpeed = 0.08;
-let autoRotate = true;
 let targetRotation = { x: 0, y: 0 };
 
 //Mouse Tracker
@@ -33,23 +32,17 @@ let cursorOnGlobe = false;
 addEventListener('mousedown', (event) => {
     {if (event.button === 0) {
         isLeftButtonDown = true;
-		autoRotate = false;
-
         previousMousePosition = {
             x: event.clientX / window.innerWidth * 2 - 1,
             y: event.clientY / window.innerHeight * 2 + 1
         };
 
         checkIfClickedOnGlobe(event);
-		if (!cursorOnGlobe) {
-            autoRotate = true;
-        }
     }}
 });
 addEventListener('mouseup', (event) => {
 	if (event.button === 0) {
 		isLeftButtonDown = false;
-		autoRotate = true;
 	}
 });
 addEventListener('mousemove', (event) => {
@@ -68,7 +61,6 @@ addEventListener('mousemove', (event) => {
 
 addEventListener('wheel', (event) => {
     if (cursorOnGlobe) {
-        autoRotate = false;
         const scrollSpeed = 0.003;
 
         const verticalChange = event.deltaY;
@@ -80,19 +72,26 @@ addEventListener('wheel', (event) => {
             event.preventDefault();
         }
 
-		setTimeout(() => {
-            autoRotate = true;
-        }, 1);
     }
 }, { passive: false });
 
-	addPing(-74.00597, 40.71427, 0x00ff00); // New York
+function addPingsWithDelay() {
+    const pingsData = [
+        { longitude: -74.00597, latitude: 40.71427, color: 0x00ff00 }, // New York
+        { longitude: 13.41053, latitude: 52.52437, color: 0x00ff00 }, // Berlin
+        { longitude: 114.16, latitude: 22.3, color: 0x00ff00 }, // Hong Kong
+        { longitude: -122.41, latitude: 37.77, color: 0x00ff00 }, // San Fran
+    ];
 
-	addPing(13.41053, 52.52437, 0x00ff00); // Berlin
+    pingsData.forEach((pingData, index) => {
+        setTimeout(() => {
+            addPing(pingData.longitude, pingData.latitude, pingData.color);
+        }, index * 2000); // Delay in milliseconds (2 seconds in this example)
+    });
+}
 
-	addPing(114.16, 22.3, 0x00ff00); // Hong Kong
-
-	addPing(-122.41, 37.77, 0x00ff00); // San Fran
+// Call the function to add pings with a delay
+addPingsWithDelay();
 
 //Animate
 camera.position.z = 15;
@@ -105,13 +104,8 @@ animate();
 
 //Functions
 function updateRotation() {
-	if (autoRotate) {
-	  globe.rotation.y += 0.0002;
-	} 
-	else {
-	  group.rotation.x += (targetRotation.x - group.rotation.x) * rotationSpeed;
-	  group.rotation.y += (targetRotation.y - group.rotation.y) * rotationSpeed;
-	}
+    group.rotation.x += (targetRotation.x - group.rotation.x) * rotationSpeed;
+    group.rotation.y += (targetRotation.y - group.rotation.y) * rotationSpeed;
 }
 
 function checkIfClickedOnGlobe(event) {
@@ -141,7 +135,27 @@ function addPing(longitude, latitude, color) {
 
     marker.position.set(x, y, z);
 
+    const target = new THREE.Vector3(x, y, z);
+
+    // Calculate rotation angles to face the ping
+    const angles = calculateRotationAngles(group.position, target);
+    targetRotation.x = angles.x;
+    targetRotation.y = angles.y;
+
+    // Update rotation immediately to face the ping
+    group.rotation.set(targetRotation.x, targetRotation.y, 0);
+
     globe.add(marker);
+}
+
+function calculateRotationAngles(currentPosition, targetPosition) {
+    const direction = new THREE.Vector3().subVectors(targetPosition, currentPosition);
+    const spherical = new THREE.Spherical().setFromVector3(direction);
+
+    return {
+        x: Math.PI / 2 - spherical.phi,
+        y: -spherical.theta,
+    };
 }
 
 function lnglatToXYZ(longitude, latitude, radius) {
