c3aa94bd3902c1aa88d040e2bfb7db4c778cd76f
LinusTC <tongchun03@gmail.com>
2025-08-17 10:32:58 +0100

    fixed traveller

--- JavaScripts/planeObject.js
+++ JavaScripts/planeObject.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 export function createTraveller (){
     const travelerGeom = new THREE.SphereGeometry(0.05, 16, 16);
     const travelerMat = new THREE.MeshBasicMaterial({ color: 0xffffff });
